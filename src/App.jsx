dd1107654621d63f3573651395989e6fcb470def
psterner <psterner@hotmail.com>
2023-11-28 15:26:00 +0100

    Remove home import

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
 import { NavBar } from "./components/NavBar/Navbar";
 import { Footer } from "./components/Footer/Footer";
-import { Home } from "./pages/Home/Home";
 
 export const App = () => {
   return (
     <BrowserRouter>
       <NavBar />
-      <Home />
-      <Footer />
       <AppRoutes />
+      <Footer />
     </BrowserRouter>
   );
 };
