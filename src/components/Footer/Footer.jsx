cee9125a96f66e8a8dd930c11814edbcd0de6e12
psterner <psterner@hotmail.com>
2023-11-29 13:18:42 +0100

    Add footer sections

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,33 @@
+import styles from "./Footer.module.scss";
+import { Link } from "react-router-dom";
+import { IconFacebook } from "../../assets/Icons";
+
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.Footer}>
+      <div className={`${styles.FooterSection} ${styles.InfoSection}`}>
+        <h3>TechCycle</h3>
+        <p>On a mission to consume less and re-use more.</p>
+        <button className={styles.FooterBtn}>
+          <Link to="/">Browse phones</Link>
+        </button>
+      </div>
+      <div className={`${styles.FooterSection} ${styles.LinksSection}`}>
+        <Link to="/">Refurbished phones</Link>
+        <Link to="/">Community market</Link>
+        <Link to="/">About</Link>
+      </div>
+      <div className={`${styles.FooterSection} ${styles.SocialSection}`}>
+        <Link to="facebook" className={styles.IconCartLink}>
+          <IconFacebook />
+        </Link>
+        <Link to="instagram" className={styles.IconCartLink}>
+          <IconFacebook />
+        </Link>
+        <Link to="linkedin" className={styles.IconCartLink}>
+          <IconFacebook />
+        </Link>
+      </div>
+    </div>
+  );
 };
