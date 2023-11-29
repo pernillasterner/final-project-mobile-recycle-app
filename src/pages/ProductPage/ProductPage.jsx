e02afcd29149ab94b7bb63274844f1a5efb9651f
psterner <psterner@hotmail.com>
2023-11-29 19:38:16 +0100

    Fetch data with specific id

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,12 +1,40 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import supabase from "../../config/supabaseClient";
 import styles from "./ProductPage.module.scss";
 
 /**
  * Use params to fetch the prodId
  * Fetch that product using supabase select
  * Update the product page with the new product
+ * http://localhost:5174/product/8
+ * 
+ * 
+
  */
 
 export const ProductPage = () => {
+  const { prodId } = useParams();
+  const [fetchError, setFetchError] = useState(null);
+  const [prod, setProd] = useState(null);
+
+  // Fetch the data with specific prodId
+  useEffect(() => {
+    const fetchProds = async () => {
+      try {
+        const { data } = await supabase.from("phones").select().eq("id", 8);
+        setProd(data[0]);
+        setFetchError(null);
+      } catch (error) {
+        setFetchError("Could not fetch products");
+        setProd(null);
+        console.log(error);
+      }
+    };
+
+    // fetchProds();
+  }, []);
+
   return (
     // TODO: Add class and set color depengin on that
     <article className={styles.ProdContainer}>
@@ -14,22 +42,31 @@ export const ProductPage = () => {
       <div className={styles.Prod__LeftColumn}>
         <div
           className={styles.ProdCardImg}
-          style={{
-            backgroundImage: `url("https://cdn.webhallen.com/images/product/326678?trim")`,
-          }}
+          style={{ backgroundImage: `url(${prod.imageUrl})` }}
         ></div>
       </div>
       <div className={styles.Prod__RightColumn}>
-        <h1 className={styles.ProdModel}>iPhone 14 Pro</h1>
+        <h1 className={styles.ProdModel}>{prod.modelValue}</h1>
+        <p>{prod.created_at}</p>
         <div className="BrandInfo">
-          <h5>Apple</h5>
-          <p>2023-10-07</p>
+          <h5>{prod.brandValue}</h5>
+          <p>{prod.priceValue} kr</p>
         </div>
         <div className="ProdDetails">
           <p>Release year: 2023</p>
         </div>
         <div className="ProdDescription">
-          <p>Description: bla bla bla</p>
+          {prod.phoneDescription.map((desc) => (
+            <>
+              <p>{desc.comment}</p>
+              <p>{desc.functionNormal ? "Ja" : "Nej"}</p>
+              <p>{desc.glassNormal ? "Ja" : "Nej"}</p>
+              <p>{desc.phoneCondition ? "Ja" : "Nej"}</p>
+              <p>{desc.phoneDamage ? "Ja" : "Nej"}</p>
+              <p>{desc.screenNormal ? "Ja" : "Nej"}</p>
+              <p>{desc.storage}</p>
+            </>
+          ))}
         </div>
         <button className="AddToCartBtn">Add to cart</button>
       </div>
