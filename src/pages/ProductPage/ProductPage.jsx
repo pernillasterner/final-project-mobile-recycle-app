a86446ab827e508d67c146729d6ca7f00f9d3bdb
psterner <psterner@hotmail.com>
2023-11-29 20:44:33 +0100

    Fetch data using id

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -14,5 +14,62 @@ import styles from "./ProductPage.module.scss";
  */
 
 export const ProductPage = () => {
-  return <div>ProductPage</div>;
+  const { prodId } = useParams();
+  const [fetchError, setFetchError] = useState(null);
+  const [prod, setProd] = useState(null);
+
+  // Fetch the data with specific prodId
+  useEffect(() => {
+    const fetchProds = async () => {
+      try {
+        const { data } = await supabase.from("phones").select().eq("id", 8);
+        setProd(data[0]);
+        setFetchError(null);
+      } catch (error) {
+        setFetchError("Could not fetch products");
+        setProd(null);
+        console.log(error);
+      }
+    };
+
+    // fetchProds();
+  }, []);
+
+  return (
+    // TODO: Add class and set color depengin on that
+    <article className={styles.ProdContainer}>
+      <div className="GoBackBtn"></div>
+      <div className={styles.Prod__LeftColumn}>
+        <div
+          className={styles.ProdCardImg}
+          style={{ backgroundImage: `url(${prod.imageUrl})` }}
+        ></div>
+      </div>
+      <div className={styles.Prod__RightColumn}>
+        <h1 className={styles.ProdModel}>{prod.modelValue}</h1>
+        <p>{prod.created_at}</p>
+        <div className="BrandInfo">
+          <h5>{prod.brandValue}</h5>
+          <p>{prod.priceValue} kr</p>
+        </div>
+        <div className="ProdDetails">
+          <p>Release year: 2023</p>
+        </div>
+        <div className="ProdDescription">
+          {prod.phoneDescription.map((desc) => (
+            <>
+              <p>{desc.comment}</p>
+              <p>{desc.functionNormal ? "Ja" : "Nej"}</p>
+              <p>{desc.glassNormal ? "Ja" : "Nej"}</p>
+              <p>{desc.phoneCondition ? "Ja" : "Nej"}</p>
+              <p>{desc.phoneDamage ? "Ja" : "Nej"}</p>
+              <p>{desc.screenNormal ? "Ja" : "Nej"}</p>
+              <p>{desc.storage}</p>
+            </>
+          ))}
+        </div>
+        <button className="AddToCartBtn">Add to cart</button>
+      </div>
+    </article>
+  );
 };
