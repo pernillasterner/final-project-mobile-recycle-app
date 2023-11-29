03e54e1441f6ae2567bd21ac95c42df2c7a2f2c2
Pernilla Sterner <psterner@hotmail.com>
2023-11-29 20:40:36 +0100

    Feature product page (#7)
    * Add z-index to nav and burger
    
    * Move main to App
    
    * Add styling to addtocard button
    
    * Add basic styling to product page
    
    * Fetch data with specific id

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -1,38 +1,18 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import supabase from "../../config/supabaseClient";
 import styles from "./ProductPage.module.scss";
 
 /**
  * Use params to fetch the prodId
  * Fetch that product using supabase select
  * Update the product page with the new product
+ * http://localhost:5174/product/8
+ * 
+ * 
+
  */
 
 export const ProductPage = () => {
-  return (
-    // TODO: Add class and set color depengin on that
-    <article className={styles.ProdContainer}>
-      <div className="GoBackBtn"></div>
-      <div className={styles.Prod__LeftColumn}>
-        <div
-          className={styles.ProdCardImg}
-          style={{
-            backgroundImage: `url("https://cdn.webhallen.com/images/product/326678?trim")`,
-          }}
-        ></div>
-      </div>
-      <div className={styles.Prod__RightColumn}>
-        <h1 className={styles.ProdModel}>iPhone 14 Pro</h1>
-        <div className="BrandInfo">
-          <h5>Apple</h5>
-          <p>2023-10-07</p>
-        </div>
-        <div className="ProdDetails">
-          <p>Release year: 2023</p>
-        </div>
-        <div className="ProdDescription">
-          <p>Description: bla bla bla</p>
-        </div>
-        <button className="AddToCartBtn">Add to cart</button>
-      </div>
-    </article>
-  );
+  return <div>ProductPage</div>;
 };
