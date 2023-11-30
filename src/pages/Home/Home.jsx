8f4b68b05ed0834fd3df7f3de2db27a8e78f7095
Sebastian Tigerschiöld <70008057+Seti108@users.noreply.github.com>
2023-11-30 13:54:30 +0100

    Feature productlist (#9)
    * Change html/classes
    
    * Add padding top for NavBar
    
    * Add ProductList component
    
    * Add basic styling
    
    * Change to use global state and add category p2p/refurb
    
    * Add category prop
    
    * Add function to update global state with products

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,10 @@
-import { Filter } from "../../components/ProductList/Filter/Filter";
 import { ProductList } from "../../components/ProductList/ProductList";
 import { HomeHero } from "../Home/HomeHero/HomeHero";
 export const Home = () => {
   return (
     <>
       <HomeHero />
-      <Filter />
-      <ProductList />
+      <ProductList category={"refurbished"} />
     </>
   );
 };
