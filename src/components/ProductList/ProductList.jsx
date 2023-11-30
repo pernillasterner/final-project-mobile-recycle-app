0415d68a308f72c53ac748962438bb24d4d71bd4
SETI108 <seti108@gmail.com>
2023-11-30 12:21:44 +0100

    Change to use global state and add category p2p/refurb

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,51 +2,78 @@ import supabase from "../../config/supabaseClient";
 import styles from "./ProductList.module.scss";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Filter } from "../../components/ProductList/Filter/Filter";
+import { useSelector, useDispatch } from "react-redux";
+import { setInitialState } from "../../reducers/productSlice";
 
-export const ProductList = () => {
+export const ProductList = ({ category }) => {
   const [fetchError, setFetchError] = useState(null);
-  const [prods, setProds] = useState(null);
-
+  // const [prods, setProds] = useState(null);
+  const prods = useSelector((state) => state.product.products);
+  const [filterProds, setFilterProds] = useState(null);
+  const dispatch = useDispatch();
+  console.log(prods);
   // Fetch the data
   useEffect(() => {
     const fetchProds = async () => {
       try {
-        const { data } = await supabase.from("phones").select(); // Get all of the data
-        setProds(data);
-        setFetchError(null);
+        if (category === "refurbished") {
+          const { data } = await supabase
+            .from("phones")
+            .select()
+            .is("peer2peer", false);
+          // setProds(data);
+          dispatch(setInitialState(data));
+          console.log(data);
+          // setFilterProds(data);
+          setFetchError(null);
+        } else if (category === "peer2peer") {
+          const { data } = await supabase
+            .from("phones")
+            .select()
+            .is("peer2peer", true);
+          // setProds(data);
+          dispatch(setInitialState(data));
+          setFilterProds(data);
+          setFetchError(null);
+        }
       } catch (error) {
         setFetchError("Could not fetch products");
-        setProds(null);
+        // setProds(null);
         console.log(error);
       }
+      console.log(prods);
     };
 
     fetchProds();
   }, []);
 
   return (
-    <div className={styles.ProductList}>
-      {fetchError && <p>{fetchError}</p>}
-      {prods && (
-        <div className={styles.FlexContainer}>
-          {prods.map((prod) => (
-            <div
-              key={prod.id}
-              className={styles.ProdCard}
-              style={{ backgroundImage: `url(${prod.imageUrl})` }}
-            >
-              <div className={styles.ProdInfo}>
-                <p>{prod.modelValue}</p>
-                <span>{prod.brandValue}</span>
-                <span>{prod.priceValue}kr</span>
-                <button className="BuyBtn">
-                  <Link to="/">Buy</Link>
-                </button>
+    <>
+      <Filter />
+      <div className={styles.ProductList}>
+        {fetchError && <p>{fetchError}</p>}
+        {prods && (
+          <div className={styles.FlexContainer}>
+            {prods.map((prod) => (
+              <div
+                key={prod.id}
+                className={styles.ProdCard}
+                style={{ backgroundImage: `url(${prod.imageUrl})` }}
+              >
+                <div className={styles.ProdInfo}>
+                  <p>{prod.modelValue}</p>
+                  <span>{prod.brandValue}</span>
+                  <span>{prod.priceValue}kr</span>
+                  <button className="BuyBtn">
+                    <Link to="/">Buy</Link>
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
