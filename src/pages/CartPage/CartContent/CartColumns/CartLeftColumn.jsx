28590f6215d66b14316b758371bf6a7c559bc91c
psterner <psterner@hotmail.com>
2023-11-30 15:37:30 +0100

    Logic to add and remove items in cart

--- src/pages/CartPage/CartContent/CartColumns/CartLeftColumn.jsx
+++ src/pages/CartPage/CartContent/CartColumns/CartLeftColumn.jsx
@@ -1,40 +1,60 @@
 import styles from "./CartLeftColumn.module.scss";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFromCart } from "../../../../reducers/cartSlice";
 import { IconRecycle, IconTrash } from "../../../../assets/Icons";
 
-export const CartLeftColumn = () => {
-  const imageUrl = "https://cdn.webhallen.com/images/product/326678?trim";
+export const CartLeftColumn = ({ cartItems }) => {
+  const dispatch = useDispatch();
+  const totalItems = useSelector((state) => state.cart.totalItems);
 
-  const handleRemoveItem = () => {
-    console.log("Remove current item from cart");
+  const handleRemoveItem = (itemId) => {
+    if (itemId) {
+      dispatch(removeFromCart(itemId));
+    }
   };
+
   return (
     <section className={styles.Cart__LeftColumn}>
       <h2 className={styles.CartTitle}>
         Cart
-        <span className={styles.CartCount}>1 produkt</span>
+        <span className={styles.CartCount}>
+          {totalItems}
+          {totalItems > 1 ? " products" : " product"}
+        </span>
       </h2>
       <div className={styles.SingleCartItem}>
-        <Link to={"/"}>
-          <div
-            className={styles.ProdCardImg}
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          ></div>
-        </Link>
-        <div className={styles.SingleCartItemInfo}>
-          <div className={styles.DetailsContainer}>
-            <div className={styles.ItemBrand}>iPhone 14 Pro</div>
-            <div className={styles.AdditionalInfo}>Model: iPhone</div>
-            <div className={styles.AdditionalInfo}>State: Okej</div>
-          </div>
+        {cartItems &&
+          cartItems.map((item, index) => (
+            <div key={index}>
+              <Link to={"/"}>
+                <div
+                  className={styles.ProdCardImg}
+                  style={{ backgroundImage: `url(${item.imageUrl})` }}
+                ></div>
+              </Link>
+              <div className={styles.SingleCartItemInfo}>
+                <div className={styles.DetailsContainer}>
+                  <p>Total Items {item.quantity}</p>
+                  <div className={styles.ItemBrand}>{item.modelValue}</div>
+                  <div className={styles.AdditionalInfo}>
+                    Brand: {item.brandValue}
+                  </div>
+                  <div className={styles.AdditionalInfo}>State: Okej</div>
+                </div>
 
-          <div className={styles.PriceContainer}>
-            <div className={styles.ItemPrice}>3999kr</div>
-            <button className="IconTrash" onClick={handleRemoveItem}>
-              {<IconTrash />}remove
-            </button>
-          </div>
-        </div>
+                <div className={styles.PriceContainer}>
+                  <div className={styles.ItemPrice}>{item.priceValue} kr</div>
+                  <button
+                    className="IconTrash"
+                    onClick={() => handleRemoveItem(item.id)}
+                  >
+                    {<IconTrash />}remove
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
       </div>
       <div className={styles.CartItemOffer}>
         <div>
--- src/pages/CartPage/CartContent/CartContent.jsx
+++ src/pages/CartPage/CartContent/CartContent.jsx
@@ -1,11 +1,13 @@
 import styles from "./CartContent.module.scss";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import { IconGoBack } from "../../../assets/Icons";
 import { CartLeftColumn } from "./CartColumns/CartLeftColumn";
 import { CartRightColumn } from "./CartColumns/CartRightColumn";
 
 export const CartContent = () => {
   const navigate = useNavigate();
+  const cartItems = useSelector((store) => store.cart.cartItems);
 
   return (
     <>
@@ -13,10 +15,15 @@ export const CartContent = () => {
         <IconGoBack />
       </div>
       <div className={styles.CartContainer}>
-        <div className={styles.CartColumns}>
-          <CartLeftColumn />
-          <CartRightColumn />
-        </div>
+        {cartItems.length !== 0 ? (
+          <div className={styles.CartColumns}>
+            <CartLeftColumn cartItems={cartItems} />
+            <CartRightColumn cartItems={cartItems} />
+          </div>
+        ) : (
+          // TODO: Prompt
+          <h1>OPPS NOTHING IN CART. PROPS PAGE</h1>
+        )}
       </div>
     </>
   );
