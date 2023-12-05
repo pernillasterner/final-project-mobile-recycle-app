94c0dd055e601c4e1d827edba56e9b0d67bf93d8
SETI108 <seti108@gmail.com>
2023-12-05 13:19:59 +0100

    Fix typo in category

--- src/pages/RefurbishedPage/RefurbishedPage.jsx
+++ src/pages/RefurbishedPage/RefurbishedPage.jsx
@@ -1,14 +1,11 @@
 import { ProductList } from "../../components/ProductList/ProductList";
 import { SmallHero } from "./SmallHero/SmallHero";
 
-import { ProductList } from "../../components/ProductList/ProductList";
-import { SmallHero } from "./SmallHero/SmallHero";
-
 export const RefurbishedPage = () => {
   return (
     <>
       <SmallHero title={"Refurbished"} />
-      <ProductList category={"Refurbished"} />
+      <ProductList category={"refurbished"} />
     </>
   );
 };
