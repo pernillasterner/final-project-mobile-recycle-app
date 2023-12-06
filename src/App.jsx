6373adee3f1f537be11292d9a66f414ff9c9e5e7
Pernilla Sterner <psterner@hotmail.com>
2023-12-06 09:30:40 +0100

    Feature general dev (#38)
    * Remove count items in cart
    
    * Add id to productlist
    
    * Comment out background image on small hero
    
    * Remove main and footer when modal is active
    
    * Simplify logic for activating modal
    
    * Simplify logic for activating modal
    
    * Clean up code. Handle thank you container
    
    * Use HashLinks to go to top of page using id
    
    * Install hashtag links
    
    * Clean up
    
    * Add id:s to each page for hashlink use

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
 import { NavBar } from "./components/NavBar/Navbar";
 import { Footer } from "./components/Footer/Footer";
+import { useSelector } from "react-redux";
 
 export const App = () => {
+  const isModalActive = useSelector((state) => state.modal.isActive);
+
   return (
     <BrowserRouter>
       <NavBar />
-      <main>
-        <AppRoutes />
-      </main>
-      <Footer />
+      {!isModalActive && (
+        <>
+          <main>
+            <AppRoutes />
+          </main>
+          <Footer />
+        </>
+      )}
     </BrowserRouter>
   );
 };
